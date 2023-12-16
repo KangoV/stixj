05c29089833fe5d07347be23ab0610efabaf849b
darren.bell <darren.bell@nominet.uk>
2023-12-16 18:51:55 +0000

    Add sighting

---
+++ src/main/java/io/kangov/stix/v21/core/sro/objects/Sighting.java
@@ -0,0 +1,115 @@
+package io.kangov.stix.v21.core.sro.objects;
+
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.kangov.stix.redaction.Redactable;
+import io.kangov.stix.util.ImmutableStyle;
+import io.kangov.stix.v21.common.type.IdentityRef;
+import io.kangov.stix.v21.common.type.SdoObjectRef;
+import io.kangov.stix.v21.core.sdo.SdoObject;
+import io.kangov.stix.v21.core.sdo.objects.Identity;
+import io.kangov.stix.v21.core.sro.SroObject;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.immutables.serial.Serial;
+import org.immutables.value.Value;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.UnaryOperator;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
+
+/**
+ * sighting
+ * <p>
+ * A Sighting denotes the belief that something in CTI (e.g., an indicator, malware, tool, threat actor, etc.) was seen.
+ * 
+ */
+@Value.Immutable
+@Serial.Version(1L)
+@ImmutableStyle
+@JsonTypeName("sighting")
+@JsonSerialize(as = Sighting.class)
+@JsonDeserialize(builder = Sighting.Builder.class)
+@JsonPropertyOrder({
+    "type",
+    "id",
+    "created_by_ref",
+    "created",
+    "modified",
+    "revoked",
+    "labels",
+    "external_references",
+    "object_marking_refs",
+    "granular_markings",
+    "first_seen",
+    "last_seen",
+    "count",
+    "sighting_of_ref",
+    "observed_data_refs",
+    "where_sighted_refs",
+    "summary"})
+@Redactable
+public interface Sighting extends SroObject {
+
+    /**
+     * Exposes the generated builder outside this package
+     * <p>
+     * While the generated implementation (and consequently its builder) is not
+     * visible outside this package, this builder inherits and exposes all public
+     * methods defined on the generated implementation's Builder class.
+     */
+    class Builder extends SightingImpl.Builder {
+        public Builder createdByRef(String id)           { return createdByRef(IdentityRef.create(id)); }
+        public Builder createdByRef(Identity identity)   { return createdByRef(IdentityRef.create(identity)); }
+        public Builder sightingOfRef(String id)          { return sightingOfRef(new SdoObjectRef(id, null)); }
+        public Builder sightingOfRef(SdoObject obj)      { return sightingOfRef(new SdoObjectRef(obj.getId(), obj)); }
+        public Builder addObservedDataRef(String id)     { return addObservedDataRef(new SdoObjectRef(id, null)); }
+        public Builder addObservedDataRef(SdoObject obj) { return addObservedDataRef(new SdoObjectRef(obj.getId(), obj)); }
+    }
+
+    static Sighting create(UnaryOperator<Builder> spec) { return spec.apply(builder()).build(); }
+    static Sighting createSighting(UnaryOperator<Builder> spec) { return create(spec); }
+    static Builder builder(UnaryOperator<Builder> spec) { return spec.apply(builder()); }
+    static Builder builder() { return new Builder(); }
+
+    default Sighting update(UnaryOperator<Builder> builder) {
+        return builder.apply(builder()).build();
+    }
+
+    @JsonProperty("first_seen")
+    @Redactable
+    Optional<Instant> getFirstSeen();
+
+    @JsonProperty("last_seen")
+    @Redactable
+    Optional<Instant> getLastSeen();
+
+    @JsonProperty("count") @JsonInclude(value = NON_EMPTY, content= NON_EMPTY)
+    @Redactable
+    Optional<@Size(max = 999999999) Integer> getCount();
+
+    @JsonProperty("sighting_of_ref")
+    @Redactable(useMask = true)
+    SdoObjectRef getSightingOfRef();
+
+    @JsonProperty("observed_data_refs") @JsonInclude(NON_EMPTY)
+    @Redactable
+    Set<SdoObjectRef> getObservedDataRefs();
+
+    @JsonProperty("where_sighted_refs") @JsonInclude(NON_EMPTY)
+    @Redactable
+    Set<SdoObjectRef> getWhereSightedRefs();
+
+    @NotNull
+    @JsonProperty("summary")
+    @Redactable
+    @Value.Default
+    default Boolean isSummary(){
+        return false;
+    }
+
+}
